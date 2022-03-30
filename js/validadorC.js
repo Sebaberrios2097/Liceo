c183cf53ff7fcc7edefa8f4df79998736f4868e1
Wingu12 <dan.gutierrezf@duocuc.cl>
2022-03-29 23:45:37 -0300

    arreglo js

--- js/validadorC.js
+++ js/validadorC.js
@@ -2,26 +2,38 @@ const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
-    nombre: /^[a-á-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    nombre: /^[a-á-zA-ZÀ-ÿ\s]{3,40}$/, // Letras y espacios, pueden llevar acentos.
     correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    asunto: /^[a-á-zA-ZÀ-ÿ-\d]{1,20}$/
 }
 
 const validarFormulario = (e) => {
     switch (e.target.name) {
         case "nombre":
-            if (expresiones.nombre.test(e.target.value)) {
-                document.getElementById('grupo__nombre').classList.remove('formulario__grupo-incorrecto')
-            } else {
-                document.getElementById('grupo__nombre').classList.add('formulario__grupo-incorrecto')
-            }
-
+            validarCampo(expresiones.nombre, e.target, 'nombre');
             break;
         case "correo":
-
+            validarCampo(expresiones.correo, e.target, 'correo');
             break;
         case "asunto":
-
+            validarCampo(expresiones.asunto, e.target, 'asunto');
             break;
+    }
+}
+
+const validarCampo = (expresion, input, campo) => {
+    if (expresion.test(input.value)) {
+        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+    } else {
+        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
     }
 }
 
